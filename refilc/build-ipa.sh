d8ce63c6b283e0e8091d8d5b538eac6c40435527
b3ni15 <ballobeni08@gmail.com>
2025-08-20 21:29:11 +0200

    Update iOS deployment target to 13.0 and bump intl package version to 0.20.2 across multiple projects

--- refilc/build-ipa.sh
+++ refilc/build-ipa.sh
@@ -16,4 +16,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-flutter build ipa --release --tree-shake-icons --split-debug-info=debug-info --obfuscate --no-codesign
+flutter build ipa --release --tree-shake-icons --split-debug-info=debug-info --obfuscate
